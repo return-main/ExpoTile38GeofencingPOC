3cbfa4ab12a545517f02cb6d5ea1c3956fedba0c
Marc PEREZ <marc.perez@epitech.eu>
2020-02-22 16:32:39 +0100

    Server was only listening locally

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const buildFastify = require('./lib/build_fastify');
 const fastify = buildFastify();
 // Run the server!
-fastify.listen(3000, (err, address) => {
+fastify.listen(3000, '0.0.0.0', (err, address) => {
   if (err) throw err;
   fastify.log.info(`server listening on ${address}`);
 });
