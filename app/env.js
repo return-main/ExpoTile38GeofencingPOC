1eb4273aae9b82600ab9fc4c3c9c26e32b47331b
Marc PEREZ <marc.perez@epitech.eu>
2020-03-14 17:58:33 +0100

    minor changes, since it's merged into Kwili, I'll remove the app

--- app/env.js
+++ app/env.js
@@ -1,4 +1,4 @@
-const HELP_API_URL = 'http://192.168.0.11:3000';
+const HELP_API_URL = 'https://geoworker.eu:3000';
 
 // eslint-disable-next-line import/prefer-default-export
 export {HELP_API_URL};
